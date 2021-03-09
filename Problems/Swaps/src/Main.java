847f072bca48acca3b7221e29f6d2ba20088ca1e
djoleK <trol.witchdoctor@gmail.com>
2021-03-09 16:35:13 +0100

    bubble sort problems solved

---
+++ Problems/Swaps/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        int[] array = new int[0];
+        if (scanner.hasNextInt()) {
+            array = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        System.out.println(countSwaps(array));
+
+    }
+
+    static int countSwaps(int[] array) {
+        int swaps = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+        return swaps;
+    }
+
+
+}
